41bd7222e7ff9bdb9142df8a682458eccfc1c428
BAH\536231 <tabit_randy@bah.com>
2017-02-02 21:56:02 -0500

    Change from chrome to phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma')
     ],
@@ -30,14 +31,17 @@ module.exports = function (config) {
       config: './angular-cli.json',
       environment: 'dev'
     },
+    junitReporter: {
+      outputDir: 'reports'
+    },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
+              ? ['progress', 'junit', 'karma-remap-istanbul']
+              : ['progress', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
