b20bfec7aa71d468ff02b8afd5cf3b3d34092f80
BAH\536231 <tabit_randy@bah.com>
2017-03-06 21:44:26 -0500

    Updating to fix compile and runtime errors

--- node-server/server.js
+++ node-server/server.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 8080));
 app.use(express.static(staticRoot));
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+    res.sendFile('dist/index.html');
 });
 
 app.listen(app.get('port'), function() {  
