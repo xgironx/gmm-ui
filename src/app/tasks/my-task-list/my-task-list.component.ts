f7923e3402224686675d8340ce7aa77df6f62936
kimberlylin <lin_kimberly@bah.com>
2017-06-28 14:19:54 -0400

    draft-task rough draft

--- src/app/tasks/my-task-list/my-task-list.component.ts
+++ src/app/tasks/my-task-list/my-task-list.component.ts
@@ -7,7 +7,8 @@ import { DateDifferencePipe } from '../../shared/pipes/date-difference.pipe';
 
 @Component({
   selector: 'app-my-task-list',
-  templateUrl: './my-task-list.component.html'
+  templateUrl: './my-task-list.component.html',
+  styleUrls: ['./my-task-list.component.css']
 })
 export class MyTaskListComponent implements OnInit {
   @Input() user:string;
