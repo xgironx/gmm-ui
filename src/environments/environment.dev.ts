2db97ca3b392cb414abba20d36725fa8ba1802ea
Joshua Rymer <rymer_joshua@bah.com>
2017-07-05 10:39:56 -0400

    changed environments.dev to reflect envirnonments.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,32 @@
+// export const environment = {
+//   production: true,
+//   serviceBase: "http://application-dev.apps.gmm.bahincubator.com:80/",
+//   serviceBaseActiviti: "http://activiti-svc-dev.apps.gmm.bahincubator.com:80/"
+// };
+
 export const environment = {
-  production: true,
-  serviceBase: "http://application-dev.apps.gmm.bahincubator.com:80/",
-  serviceBaseActiviti: "http://activiti-svc-dev.apps.gmm.bahincubator.com:80/"
+  production: false,
+  serviceBase: "http://data-dev.apps.gmm.bahincubator.com/swagger-ui.html", //"http://application-dev.apps.gmm.bahincubator.com/","http://localhost:8402/",
+  serviceBaseActiviti: "http://data-dev.apps.gmm.bahincubator.com/",
 };
+
+export const serviceRoutes = {
+  swagger: {
+    applicationForm: {
+      submit: environment.serviceBase + '/submit'
+    },
+    grants: {
+      getGrantTypes: environment.serviceBase + '/getGrantTypes',
+      getApplicationForm: environment.serviceBase + '/getAppForm'
+    }
+  },
+  activiti: {
+    applicationForm: {
+      submit: environment.serviceBase + '/submit'
+    },
+    grants: {
+      getGrantTypes: environment.serviceBase + '/getGrantTypes',
+      getApplicationForm: environment.serviceBase + '/getAppForm'
+    }
+  }
+}
